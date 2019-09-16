15aa8f3c3c74f61ebea6730a7e3f59592aa128ad
Eduardo Antonio Fernandez Diaz <fernandez9000@gmail.com>
2019-09-16 12:27:51 -0500

    value component deleted

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,14 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  values: any;
   registerMode = false;
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getValues();
   }
 
   RegisterToggle() {
     this.registerMode = !this.registerMode;
+  }
+
+  getValues() {
+    this.http.get('http://localhost:5000/api/values/').subscribe(
+      response => {
+        this.values = response;
+      }, error => {
+        console.log(error);
+      });
   }
 }
