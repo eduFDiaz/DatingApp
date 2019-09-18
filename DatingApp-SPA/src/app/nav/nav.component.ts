1c44841de26169aedcb5a9ecb355c8bb0d441e32
Eduardo Antonio Fernandez Diaz <fernandez9000@gmail.com>
2019-09-18 13:43:20 -0500

    user is routed to members when logged in and to home when logged out now in navbar

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -15,7 +16,7 @@ export class NavComponent implements OnInit {
     password: 'password'
   };
 
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -26,6 +27,8 @@ export class NavComponent implements OnInit {
       this.alertify.success('Logged in successfully!');
     }, error => {
       this.alertify.error(error);
+    }, () => {
+      this.router.navigate(['members']);
     });
   }
 
@@ -36,6 +39,7 @@ export class NavComponent implements OnInit {
   Logout() {
     this.authService.Logout();
     this.alertify.message('Logged out');
+    this.router.navigate(['home']);
   }
 
   changeTheme(): void {
