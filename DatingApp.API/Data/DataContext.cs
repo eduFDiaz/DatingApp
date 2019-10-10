fa8c0c4e84d1a79f8fe51b4fb4222877d0247f5f
Eduardo Antonio Fernandez Diaz <fernandez9000@gmail.com>
2019-10-09 19:03:14 -0500

    [API] Message Entity created

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -9,13 +9,14 @@ namespace DatingApp.API.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Photo> Photos { get; set; }
         public DbSet<Like> Likes { get; set; }
+        public DbSet<Message> Messages { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             // make key a combination of both keys to ensure
             // that a user is only liked once by another user
             builder.Entity<Like>()
-            .HasKey(k => new {k.LikerId, k.LikeeId});
+                .HasKey(k => new {k.LikerId, k.LikeeId});
 
             // A user can like many users part of the relationship
             builder.Entity<Like>()
@@ -29,6 +30,21 @@ namespace DatingApp.API.Data
                 .HasOne(u => u.Liker)
                 .WithMany(u => u.Likeers)
                 .HasForeignKey(u => u.LikerId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            ////////////////////////////////////////////////////////////////////////////////
+            //////////// Defining Messages relationships using fluent API //////////////////
+            ////////////////////////////////////////////////////////////////////////////////
+            // A sender can have many MessagesSent (A message has a sender and can send many messages)
+            builder.Entity<Message>()
+                .HasOne(m => m.Sender)
+                .WithMany(m => m.MessagesSent)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // A Recipient can have many MessagesReceived (A message has a recipient and can send many messages)
+            builder.Entity<Message>()
+                .HasOne(m => m.Recipient)
+                .WithMany(m => m.MessagesReceived)
                 .OnDelete(DeleteBehavior.Restrict);
         }
     }
